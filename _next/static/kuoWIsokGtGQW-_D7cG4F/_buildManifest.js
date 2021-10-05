03b2f51a4694cf805c97a16c8f1b35363751a77c
acedev0816 <60019010+acedev1210@users.noreply.github.com>
2021-10-05 18:56:48 +0800

    Updates

--- _next/static/kuoWIsokGtGQW-_D7cG4F/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-d6a0cda990f652729ff3.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
